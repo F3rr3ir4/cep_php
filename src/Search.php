2360aca7b640f91940f17312d78b4835ce46b9a7
F3rr3ir4 <leandroferreira757@msn.com>
2019-12-12 22:39:04 -0300

    criado search

---
+++ src/Search.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wead\CepPhp
+
+class Serach{
+    private $url "https://viacep.com.br/ws/";
+
+    public function getAddFromZipcode(string $ZipCode): array{
+        $ZipCode = preg_replace('//')
+    }
+}
