46762d6459c772abd55ba64d2caf45fd0503f168
F3rr3ir4 <leandroferreira757@msn.com>
2019-12-20 20:31:34 -0300

    codeSniffer

--- src/Search.php
+++ src/Search.php
@@ -6,11 +6,12 @@ class Search
 {
     private $url = "https://viacep.com.br/ws/";
 
-    public function getAddressFromZipcode(string $ZipCode): array{
-        $ZipCode = preg_replace('/[^0-9]/im','', $ZipCode);
+    public function getAddressFromZipcode(string $ZipCode): array
+    {
+        $ZipCode = preg_replace('/[^0-9]/im', '', $ZipCode);
 
-        $get = file_get_contents($this->url . $ZipCode . "/json");   
+        $get = file_get_contents($this->url . $ZipCode . "/json");
         
-       return(array) json_decode($get);
+        return(array) json_decode($get);
     }
 }
--- src/exemplo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Wead\CepPhp\Search;
-
-$busca = new Search;
-
-$resultado = $busca->getAddressFromZipcode('89074605');
-
-print_r($resultado);
